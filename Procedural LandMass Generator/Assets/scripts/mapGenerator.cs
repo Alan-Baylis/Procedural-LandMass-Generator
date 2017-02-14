dd961bc16671d601a76b968b306e6b893588d866
Daniel Aber <sshrugg@gmail.com>
2017-02-14 06:16:29 -0800

    Perlin Noise Generator
    -Created Noise Generator
    -Public variables for width, height, noiseScale
    -Custom editor draws without executing and auto updates scene view on
    changes (public bool to disable)

---
+++ Procedural LandMass Generator/Assets/scripts/mapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class mapGenerator : MonoBehaviour {
+    public int mapHeight;
+    public int mapWidth;
+    public float scale;
+    public bool autoUpdate;
+
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = noise.GenerateNoiseMap(mapWidth, mapHeight, scale);
+
+        mapDisplay display = FindObjectOfType<mapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+	
+}
